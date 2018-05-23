fa783580a1ba685ae7769840cf414ca08ea354d0
Camille Vo <camillevo@garagescript.org>
2018-05-22 19:26:58 -0700

    3.js done

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -1,12 +1,14 @@
 /**
  * Say Hello - takes in a number x
  *     return string with “hello” repeated x times.
- * @param {number} a
+ * @param {number} x
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (x, numTimesCalled=0,result="") => {
+  if(x===numTimesCalled) return result;
+  result=result+'hello'
+  return solution(x, numTimesCalled+1,result);
 };
 
 module.exports = {
