e752d8841bcdabe53eab0de264eaf65fef6f2fa3
Camille Vo <camillevo@garagescript.org>
2018-06-14 16:38:53 -0700

    js3 1.js Max Object

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -1,12 +1,17 @@
 /**
+ *
  * biggestValue - takes in object, returns largest value
  * @param {object} obj
  * @returns {number} max
  */
 
-const solution = (obj)=>{
-  return 0;
-}
+const solution = (obj, keys = Object.keys(obj), max = obj[keys[0]], i = 1) => {
+  if (i === keys.length) return max;
+  if (obj[keys[i]] > max) {
+    max = obj[keys[i]];
+  };
+  return solution(obj, keys, max, i + 1);
+};
 
 module.exports = {
   solution
