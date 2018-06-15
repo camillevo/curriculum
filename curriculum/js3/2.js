b1d8f4344246c959980d5c5b624a51526af041a5
Camille Vo <camillevo@garagescript.org>
2018-06-14 18:28:22 -0700

    js3 2.js Common Array Element

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -4,10 +4,22 @@
  * @return {number}
  */
 
-const solution = (arr)=>{
-  return 0;
+const solution = (arr, tally = {}, i = 0)=>{
+  const  max = (keys = Object.keys(tally), common = keys[0], p = 1)=>{
+    if(p === keys.length) return +common;
+    if (tally[keys[p]] > tally[common]) {
+      common = keys[p];
+    }
+    return max(keys, common, p + 1);
+  }
+  if(i === arr.length) return max();
+  if(tally[arr[i]] == undefined) {
+    tally[arr[i]] = 1;
+    return solution(arr, tally, i + 1);
+  }
+  tally[arr[i]] += 1;
+  return solution(arr, tally, i + 1);
 };
-
 module.exports = {
   solution
 }
