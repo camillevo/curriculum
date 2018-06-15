0704fc09f15cc10199e5911ae9492c21e1c6a1c1
Camille Vo <camillevo@garagescript.org>
2018-06-14 19:01:54 -0700

    js3 3.js Array of Repeats

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -4,10 +4,23 @@
  * @return {array} b
  */
 
-const solution = (a)=>{
-  return b;
+const solution = (a, sample = {}, i = 0)=>{
+  const func = (keys = Object.keys(sample), b = [], p = 0)=>{
+    if(p === keys.length) return b;
+    if(sample[keys[p]] == 2) {
+      b.push(+keys[p]);
+    }
+    return func(keys, b, p + 1);
+  }
+  
+  if(i === a.length) return func();
+  if(sample[a[i]] == undefined) {
+    sample[a[i]] = 1;
+    return solution(a, sample, i + 1);
+  }
+  sample[a[i]] = 2;
+  return solution(a, sample, i + 1)
 }
-
 module.exports = {
   solution
 }
