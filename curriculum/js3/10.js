0f5e23454134123d1373eef82816d326fe13b630
Camille Vo <camillevo@garagescript.org>
2018-06-20 22:39:40 -0700

    js3 10.js Hard One

--- curriculum/js3/10.js
+++ curriculum/js3/10.js
@@ -20,7 +20,7 @@
 // This problem is pretty hard, you just have
 // to understand whats going on and look at it over
 // and over again until you can write it out yourself
-const bft = (cur=[], next=[], result=[]) => {
+/*const bft = (cur=[], next=[], result=[]) => {
   if (!cur.length && !next.length) return result;
   if (!cur.length) {
     result.push([]);
@@ -29,10 +29,35 @@ const bft = (cur=[], next=[], result=[]) => {
   const n = cur.shift();
   result[result.length-1].push(n.v);
   return bft(cur, next.concat(n.children), result);
-};
+};*/
+
+bft = (n, kids = n.children, nextGen = [], level = [], res = []) => {
+  if(res.length === 0) {
+    res.push([n.v]);
+  }
+  if(kids.length === 0 && nextGen.length === 0) {
+    if(level.length != 0) { res.push(level);}
+    return res;
+  }
+  if(kids.length === 0) {
+    res.push(level);
+    kids = kids.concat(nextGen);
+    return bft(n, kids, nextGen = [], level = [], res);
+            }
+      curr = kids.shift();
+      level.push(curr.v);
+      if(curr.children) {
+                nextGen = nextGen.concat(curr.children);
+            };
+     console.log("kids ", kids);
+     console.log("curr", curr);
+     console.log("level", level);
+     console.log("res ", res);
+      return bft(n, kids, nextGen, level, res);
+}
 
 const solution = (n)=>{
-  return bft([], [n]);
+  return bft(n);
 };
 
 module.exports = {
